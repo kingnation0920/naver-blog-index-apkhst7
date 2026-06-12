1e1d0d0af5db341b5b754d54363e9c055d66d6f6
kingnation0920 <57610017+kingnation0920@users.noreply.github.com>
2026-06-12 20:01:21 +0900

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 export const metadata = {
   title: "어제보다 행복한 오늘 사람과성장 블로그 글 모음",
   description: "어제보다 행복한 오늘 사람과성장 네이버 블로그 글 모음 - 최신 글 목록을 자동으로 보여줍니다.",
+  verification: {
+    google: "DygTh4U9GSwzJ_41exp75QLL3boNvWGo7h80IOdVkY0",
+  },
 };
 
 export default function RootLayout({ children }) {
